3857f049c3eabc5db191e574f54611b7449a4a65
rkeval <r.keval@gmail.com>
2017-02-11 01:15:14 -0800

    Initial commit

---
+++ app/src/main/java/android/csulb/edu/temperatureconverter/ConverterUtil.java
@@ -0,0 +1,16 @@
+package android.csulb.edu.temperatureconverter;
+
+/**
+ * Created by Keval on 10-02-2017.
+ */
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
